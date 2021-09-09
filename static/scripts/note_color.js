2d988848387aafbf91b3bba9849325779ef7c69e
Arjun31415 <arjunp0710@gmail.com>
2021-09-09 11:25:07 +0530

    Added comments

--- static/scripts/note_color.js
+++ static/scripts/note_color.js
@@ -4,9 +4,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
 var xncolorpicker
 
+// Function to create the colorPicker
 function colorPicker() {
+    // change the background of the form to default color
     document.getElementsByClassName('new-note-form')[0].style.backgroundColor = isDark ? "#202124" : "#ffffff";
 
+    // Create the color picker
     xncolorpicker = new XNColorPicker({
         selector: "#colorpicker",
         showPalette: false,
@@ -30,12 +33,11 @@ function colorPicker() {
             let elem = document.getElementsByClassName('new-note-form')[0];
             console.log(typeof (color.color.hex), color.color.hex);
             elem.style.backgroundColor = color.color.hex;
-
         },
-
         // customize your colors here
         prevcolors: (isDark ? darkColors : lightColors)
     });
+    // Add border and shadow to the colorPicker div
     document.getElementsByClassName('fcolorpicker-curbox')[0].classList.add('border');
     document.getElementsByClassName('fcolorpicker-curbox')[0].classList.add(isDark ? 'myShadowDark' : 'myShadowLight');
     console.log(
@@ -46,18 +48,21 @@ function colorPicker() {
             "background-color"
         )
     );
+    // Add click event listener to the color picker so that 
+    // the input box showing the value of the color is disabled,
+    // this ensures that user cannot change the color outside the pallete.
     target = document.getElementsByClassName('fcolorpicker-curbox')[0];
     target.addEventListener("click", function (event) {
+        // select the inputbox to be disabled
         ipBox = document.querySelector(".current-color-value > input:nth-child(1)");
-        console.log(ipBox);
         ipBox.disabled = true;
+        // A few CSS effects because the padding and 
+        // the box position is irregular and it bugs me
+        document.querySelector(".current-color-value").style.padding = "0 0";
         ipBox.style.height = "23px";
         ipBox.style.width = "100%";
         ipBox.style.padding = "1px 0";
         ipBox.style.textAlign = "center";
-        document.querySelector(".current-color-value").style.padding = "0 0";
-
-
     })
 
 }
