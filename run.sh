6ec618b74c16fa05148656b5ae982ca3bd825821
Arjun31415 <arjunp0710@gmail.com>
2021-10-06 13:38:44 +0530

    removed unnecessary environment variable

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # this is for development and debugging the code not for deployment
 export FLASK_APP=app.py
-export FLASK_DEBUG=true
 export FLASK_ENV=development
 export FLASK_DEBUG=1
 
